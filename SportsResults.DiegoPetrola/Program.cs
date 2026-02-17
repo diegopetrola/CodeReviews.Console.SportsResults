69869e0ed54c3915e56be67454458fbe241adee7
Diego Leal <diegopetrola@gmail.com>
2026-02-17 00:11:19 -0300

    init

---
+++ SportsResults.DiegoPetrola/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration.AddUserSecrets<Program>();
+
+IHost host = builder.Build();
+
+host.Run();
